225855aae45bf2eb327c498640a147c0bea555ae
Michael Lackey <michael.g.lackey@gmail.com>
2022-04-06 17:25:30 -0500

    update to React 18

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './styles/App.css'
-import React from 'react'
 
 const App = () => {
+
   return (
     <div className="App">
       <h1>Movie List</h1>
     </div>
   )
-};
+}
 
-export default App;
+export default App
